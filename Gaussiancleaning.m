3b9d4a8412324c4fd5ba502673bf2af1b61fad0f
Andrés <andresgonzalez@uma.es>
2017-10-25 21:38:55 +0200

    Initial commit
    Let's go!

---
+++ Gaussiancleaning.m
@@ -0,0 +1,11 @@
+original = imread('mijeta.jpg');
+original = rgb2gray( original(:,:,1:3) );
+gnoise = imnoise(original,'gaussian');
+subplot(2,2,1); imshow(gnoise); title('Gaussian noise');
+f = ones(3,3) / 9;
+avgs = imfilter(gnoise,f);
+subplot(2,2,2); imshow(avgs); title('Averaging smoothing');
+medians = medfilt2(gnoise,[3 3]);
+subplot(2,2,3); imshow(medians); title('Median smoothing');
+gsmoothing = imgaussfilt(gnoise);
+subplot(2,2,4); imshow(gsmoothing); title('Gaussian smoothing'); 
