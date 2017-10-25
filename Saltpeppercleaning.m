b5fe23a2ec261e6058ba8f12f73f77be6f357ab8
Andrés <andresgonzalez@uma.es>
2017-10-25 21:48:37 +0200

    Update Saltpeppercleaning.m

--- Saltpeppercleaning.m
+++ Saltpeppercleaning.m
@@ -1,4 +1,6 @@
-original = imread('mijeta.jpg');
+% this script adds Salt & Pepper noise to an image and then 
+% cleans it by averaging, median and gaussian filters
+original = imread('photo.jpg');
 original = rgb2gray( original(:,:,1:3) );
 spnoise = imnoise(original,'salt & pepper');
 subplot(2,2,1); imshow(spnoise); title('Salt & Pepper noise');
