7346f9501ca6b9649a01356f9bc44886931904ba
Datalift <andresgonzalez@uma.es>
2017-11-22 18:43:16 +0100

    Working on Feature Extraction
    Feature extraction applied to the classification of objects

---
+++ Feature-extraction/central_moments.m
@@ -0,0 +1,7 @@
+function [mu00,mu10,mu01,mu11,mu20,mu02,mu21,mu12,mu30,mu03] = central_moments([m00,m02,m11,m12,m20,m21,m30],I)
+%CENTRAL_MOMENTS Computes the central moment of a grayscale image    
+%   
+
+
+end
+
---
+++ Feature-extraction/raw_moments.m
@@ -0,0 +1,6 @@
+function [m00,m02,m11,m12,m20,m21,m30] = raw_moments(I)
+%RAW_MOMENTS Is used to calculate the raw moments of an image to prepare
+% a feature extraction
+%   Detailed explanation goes here
+end
+
