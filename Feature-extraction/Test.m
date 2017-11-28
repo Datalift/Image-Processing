620abadf543f095dd530777d500c0e3b5e03ba57
Datalift <andresgonzalez@uma.es>
2017-11-28 18:37:31 +0100

    Feature extraction update

--- Feature-extraction/Test.m
+++ Feature-extraction/Test.m
@@ -1,4 +1,3 @@
 I = imread('botella_A_1.bmp');
 imshow(I);
-plot(x.^3);
-%central_moments(I);
+central_moments(I);
--- Feature-extraction/central_moments.m
+++ Feature-extraction/central_moments.m
@@ -4,7 +4,7 @@ function [m,m10,m01,m11,m20,m02,m21,m12,m30,m03] = central_moments(I)
 %   The input of the function is an image to which we want to calculate the
 %   central moments, we are going to use raw_moments() function to make
 %   this step easier.
-[rm,rm01,rm02,rm10,rm11,rm12,rm20,rm21,rm30] = raw_moments(I);
+[rm,rm01,rm02,rm10,rm11,rm12,rm20,rm21,rm30,rm03] = raw_moments(I);
 %The centroid is calculated
 [maxx, maxy] = size(I);
 x_ = rm10/rm;
@@ -20,12 +20,12 @@ m20 = rm20 - m * x_.^2;
 m11 = rm20 - m * x_ * y_;
 m02 = rm02 - m * y_.^2;
 m30 = rm30 - 3*rm20 * x_ + 2*m * x_.^3;
+m21 = 0;
+m12 = 0;
 m03 = rm03 - 3*rm02*y_ + 2*m*y_.^3;
 for y=1:maxy
     for x=1:maxx
-        
+        m21 = m21 + (x-x_).^2*(y-y_).^1*I(x,y);
+        m12 = m12 + (x-x_).^1*(y-y_).^2*I(x,y);
     end
 end
-
-
-
