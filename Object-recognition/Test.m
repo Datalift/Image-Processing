f16570676b0c46f75967ac87f0c990a7c374cc85
Datalift <andresgonzalez@uma.es>
2018-01-09 18:21:45 +0100

    He terminado la Visualización
    El clasificador Bayesiano ya permite hacer la visualizacion de las distintas clases.

--- Object-recognition/Test.m
+++ Object-recognition/Test.m
@@ -1,18 +1,28 @@
+imcell = cell(15,1);
 for i = 1 : 15
   filename = strcat('botella_A_',strcat(num2str(i),'.bmp'));
-  I = imread(filename);
-  HA(i) = momentos_Hu(I);
+  image = imread(filename);
+  imcell{i} = image;
 end
+[centroid, covar_matrix] = stat_params(imcell);
+hold on
+Visualization(centroid, covar_matrix, 'ro');
+hold off
 for i = 1 : 15
   filename = strcat('botella_B_',strcat(num2str(i),'.bmp'));
-  I = imread(filename);
-  HB(i) = momentos_Hu(I);
+  image = imread(filename);
+  imcell{i} = image;
 end
+[centroid, covar_matrix] = stat_params(imcell);
+hold on
+Visualization(centroid, covar_matrix, 'ro');
+hold off
 for i = 1 : 15
   filename = strcat('botella_C_',strcat(num2str(i),'.bmp'));
-  I = imread(filename);
-  HC(i) = momentos_Hu(I);
+  image = imread(filename);
+  imcell{i} = image;
 end
-disp(HA);
-disp(HB);
-disp(HC);
+[centroid, covar_matrix] = stat_params(imcell);
+hold on
+Visualization(centroid, covar_matrix, 'ro');
+hold off
--- Object-recognition/stat_params.m
+++ Object-recognition/stat_params.m
@@ -1,33 +1,29 @@
-function [ centroid, covar_matrix ] = stat_params( imvect )
+function [ centroid, covar_matrix ] = stat_params( imcell )
 %STAT_PARAMS Computes the statistical parameters of Hu moments of 15 images
 %   We are going to compute the centroid in 7 dimensions of the Hu
 %   parameters of 15 images of the same class, as well as the covar for
 %   that same class.
 %   Andrés González 2017.
-HM = zeros(15);
+HM = zeros(15,7);
 for x = 1:15
-    HM(x) = momentos_Hu(imvect(x));
-end
-%Calculating centroid in 7 dimensions
-centroid = zeros(7,1);
-for x = 1:7
-    for y = 1:15
-        centroid(x) = centroid(x) + HM(x,y);
+    I = imcell{x};
+    auxvect = momentos_Hu(I);
+    for y = 1:7
+        HM(x,y) = auxvect(y);
     end
-end
-for x = 1:7
-    centroid(x) = centroid(x)./15;
 end
 % Using the Hu moments of 15 images we calculate the Mu 
-mu = zeros(7,1);
-for y = 1:15
-    for x = 1:7
-        mu(x) = mu(x) + HM(x,y); 
+mu = zeros(15,1);
+for x = 1:15
+    for y = 1:7
+        mu(x) = mu(x) + HM(x,y);
     end
 end
+centroid = mu;
 for x = 1:15
     mu(x) = mu(x)./15;
 end
+% Calculation of the expected result.
 expected = 0;
 for x = 1:15
     expected = expected + mu(x);
@@ -39,7 +35,7 @@ covar_matrices = zeros(7,7,15);
 for im = 1:15
     for i = 1:7
         for j = 1:7
-            op = (HM(i,im)-mu(i))*(HM(j,im)-mu(j));
+            op = (HM(im,i)-mu(i))*(HM(im,j)-mu(j));
             covar_matrices(i,j,im) = expected*op;
         end
     end
