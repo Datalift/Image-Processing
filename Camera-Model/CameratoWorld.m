8142b884d7b04e1de06418bf8049ddc3a02ddf06
Datalift <andresgonzalez@uma.es>
2018-01-15 17:01:55 +0100

    Homography works
    I have to find a reliable way to tell the computer which is the first point to signal, but the homography is up and ready

--- Camera-Model/CameratoWorld.m
+++ Camera-Model/CameratoWorld.m
@@ -5,8 +5,8 @@ ky = 1;
 tita = 90;
 x0 = 319.5;
 y0 = 239.5;
-xmax = x0*2;
-ymax = y0*2;
+xmax = x0*2+1;
+ymax = y0*2+1;
 image_RGB = imread('person_rgb.png');
 imshow(image_RGB);
 pause(1);
@@ -18,9 +18,9 @@ X = zeros(xmax,ymax);
 Y = zeros(xmax,ymax);
 Z = image_depth./5000;
 
-for xp = 1:639 %x prime
-    for yp = 1:480 %y prime
-        lambda = image_depth(xp,yp);
+for xp = 1:xmax %x prime
+    for yp = 1:ymax %y prime
+        lambda = image_depth(yp,xp);
         X(xp,yp) = lambda*((xp-x0)/f);
         Y(xp,yp) = lambda*((yp-y0)/f);
     end
